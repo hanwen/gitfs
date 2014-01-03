f1dfa317a388457330bda1f807afa9dce0f7dd41
Han-Wen Nienhuys <hanwen@google.com>
2014-01-03 22:27:03 +0100

    Make main driver use multifs.

--- main.go
+++ main.go
@@ -8,37 +8,26 @@ import (
 	
 	"github.com/hanwen/gitfs/fs"
 	"github.com/hanwen/go-fuse/fuse/nodefs"
-
-	git "github.com/libgit2/git2go"
 )
 
 func main() {
-	tree := flag.String("tree", "master", "tree to mount")
 	flag.Parse()
-	
-	if len(flag.Args()) < 2 {
-		log.Fatalf("usage: %s REPO MOUNT", os.Args[0])
+	if len(flag.Args()) < 1 {
+		log.Fatalf("usage: %s MOUNT", os.Args[0])
 	}
 
-	repoDir := flag.Args()[0]
-	mntDir := flag.Args()[1]
+	mntDir := flag.Args()[0]
 
-	repo, err := git.OpenRepository(repoDir)
-	if err != nil {
-		log.Fatalf("OpenRepository(%q): %v", repoDir, err)
-	}
-
-	fs, err := fs.NewTreeFS(repo, *tree)
-	if err != nil {
-		log.Fatalf("NewTreeFS(%q): %v", *tree, err)
-	}
-
+	fs := fs.NewMultiGitFS()
 	server, _, err := nodefs.MountFileSystem(mntDir, fs, &nodefs.Options{
 		EntryTimeout: time.Hour,
 		NegativeTimeout: time.Hour,
 		AttrTimeout: time.Hour,
 		PortableInodes: true,
 	})
-	log.Printf("Started gitfs FUSE on %s", mntDir)
+	if err != nil {
+		log.Fatalf("MountFileSystem: %v", err)
+	}
+	log.Printf("Started git multi fs FUSE on %s", mntDir)
 	server.Serve()
 }
